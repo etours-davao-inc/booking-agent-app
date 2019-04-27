66001110175d5d0eb5acfa4c7d5ddf89053621f0
Tom Claudio <>
2019-04-28 02:31:42 +0800

    Extract Styled-Components

---
+++ app/src/BookingForm/Styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FormField = styled.div`
+  display: flex;
+  align-items: center;
+  input, label {
+    color: #2f3542;
+  }
+  label {
+    width: 200px;
+  }
+  margin-bottom: 10px;
+`
+
+export const FormSection = styled.section`
+padding: 10px; 
+`

a2340c90ed37ee72b707d8071c2e1c39c5b89165
Tom Claudio <>
2019-04-27 17:17:30 +0800

    Put proper label and input names, ids

--- app/src/BookingForm/index.js
+++ app/src/BookingForm/index.js
@@ -28,7 +28,7 @@ padding: 10px;
 export default props => (
   <React.Fragment>
     
-    <FormStyle classNameName="pure-form">
+    <FormStyle className="pure-form">
       <h1>Booking Form</h1>
       <FormSection>
         <FormField>
@@ -46,115 +46,115 @@ export default props => (
           <input id="name" type="text" name="name" size={60}/>
         </FormField>
         <FormField>
-          <label htmlFor="name">Contact No: </label>
-          <input id="name" type="text" name="contact" size={60} />
+          <label htmlFor="contact">Contact No: </label>
+          <input id="contact" type="text" name="contact" size={60} />
         </FormField>
         <FormField>
-          <label htmlFor="name">Email: </label>
-          <input id="name" type="text" name="email" size={60} />
+          <label htmlFor="email">Email: </label>
+          <input id="email" type="text" name="email" size={60} />
         </FormField>
       </FormSection>
       <FormSection>
         <h2>Guest Information</h2>
         <FormField>
-          <label htmlFor="name">Number of Adults: </label>
-          <input id="name" type="text" name="name" size={3} />
+          <label htmlFor="adults">Number of Adults: </label>
+          <input id="adults" type="text" name="adults" size={3} />
         </FormField>
         <FormField>
-          <label htmlFor="name">Number of kids(0-2): </label>
-          <input id="name" type="text" name="name" size={3} />
+          <label htmlFor="kid02">Number of kids(0-2): </label>
+          <input id="kid02" type="text" name="kid02" size={3} />
           </FormField>
         <FormField>  
-          <label htmlFor="name">Number of kids(3-5): </label>
-          <input id="name" type="text" name="name" size={3} />
+          <label htmlFor="kid35">Number of kids(3-5): </label>
+          <input id="kid35" type="text" name="kid35" size={3} />
           </FormField>
         <FormField>
-          <label htmlFor="name">Number of kids 6-11: </label>
-          <input id="name" type="text" name="name" size={3} />
+          <label htmlFor="kid611">Number of kids 6-11: </label>
+          <input id="kid611" type="text" name="kid611" size={3} />
         </FormField>
         <FormField>
-          <label htmlFor="name">Arrival Date: </label>
+          <label htmlFor="">Arrival Date: </label>
           <fieldset className="pure-group">
-            <input id="name" type="text" name="email" />
-            <input id="name" type="text" name="contact" placeholder="Flight Number"/>
+            <input id="from" type="text" name="from" />
+            <input id="from_flight_no" type="text" name="from_flight_no" placeholder="Flight Number"/>
           </fieldset>
         </FormField>
         <FormField>
-          <label htmlFor="name">Departurn Date: </label>
+          <label htmlFor="">Departurn Date: </label>
           <fieldset className="pure-group">
-            <input id="name" type="text" name="email" />
-            <input id="name" type="text" name="contact" placeholder="Flight Number"/>
+            <input id="to" type="text" name="to" />
+            <input id="to_flight_no" type="text" name="to_flight_no" placeholder="Flight Number"/>
           </fieldset>
         </FormField>
         <FormField>
-          <label htmlFor="name">Tour Date: </label>
+          <label htmlFor="">Tour Date: </label>
           <fieldset className="pure-group">
-            <input id="name" type="text" name="email" />
-            <input id="name" type="text" name="contact" placeholder="Start time"/>
-            <input id="name" type="text" name="contact" placeholder="End time"/>
+            <input id="tourdate" type="text" name="tourdate" />
+            <input id="from" type="text" name="from" placeholder="Start time"/>
+            <input id="to" type="text" name="to" placeholder="End time"/>
           </fieldset>
         </FormField>        
       </FormSection>
-      <FormSection id="guest-info">
+      <FormSection>
         <h2>Accommmodation Info</h2>
         <FormField>
-          <label htmlFor="name">Hotel: </label>
-          <input id="name" type="text" name="name" size={50} />
+          <label htmlFor="hotel">Hotel: </label>
+          <input id="hotel" type="text" name="hotel" size={50} />
         </FormField>
         <FormField>
-          <label htmlFor="name">Reservation number</label>
-          <input id="name" type="text" name="contact"/>
+          <label htmlFor="hotel_reservation_number">Reservation number</label>
+          <input id="hotel_reservation_number" type="text" name="hotel_reservation_number"/>
         </FormField>
         <FormField>
-          <label htmlFor="name">Room number </label>
-          <input id="name" type="text" name="contact" size={5} />
+          <label htmlFor="hotel_room_number">Room number </label>
+          <input id="hotel_room_number" type="text" name="hotel_room_number" size={5} />
         </FormField>
         <FormField>  
-          <label htmlFor="name">Reservation Type</label>
+          <label htmlFor="">Reservation Type</label>
           <select id="booking-type">
             <option>Etours Account</option>
             <option>Guest Account</option>
           </select>
         </FormField>      
       </FormSection>
-      <FormSection id="guest-info">
+      <FormSection>
         <h2>Airport Pickup</h2>
         <FormField>
-          <label htmlFor="name">With Airport Pickup</label>
-          <input type="checkbox" />
+          <label htmlFor="with_airport_pickup">With Airport Pickup</label>
+          <input type="checkbox" name="with_airport_pickup" checked />
         </FormField>
       </FormSection>
-      <FormSection id="guest-info">
+      <FormSection>
         <h2>Tourguide</h2>
-        <select id="booking-type">
+        <select id="tourguide">
           <option>Sam</option>
           <option>Jojo</option>
           <option>Milber</option>
           <option>Shagay</option>
         </select>    
       </FormSection>
-      <FormSection id="guest-info">
+      <FormSection>
         <h2>Vehicle</h2>
         <FormField>
-          <label htmlFor="name">Operator: </label>
-          <input id="name" type="text" name="contact" />
+          <label htmlFor="vehicle_operator">Operator: </label>
+          <input id="vehicle_operator" type="text" name="vehicle_operator" />
         </FormField>
         <FormField>    
-          <label htmlFor="name">Vehicle: </label>
+          <label htmlFor="">Vehicle: </label>
           <fieldset className="pure-group">
-            <input id="name" type="text" name="contact" placeholder="Type"/>
-            <input id="name" type="text" name="contact" placeholder="Plate Number"/>
+            <input id="vehicle_operator" type="text" name="vehicle_operator" placeholder="Type"/>
+            <input id="vehicle_plate_no" type="text" name="vehicle_plate_no" placeholder="Plate Number"/>
           </fieldset>
         </FormField>
         <FormField>    
-          <label htmlFor="name">Driver: </label>
+          <label htmlFor="">Driver: </label>
           <fieldset className="pure-group">
-            <input id="name" type="text" name="contact" placeholder="Name"/>
-            <input id="name" type="text" name="contact" placeholder="Contact"/>
+            <input id="vehicle_driver" type="text" name="vehicle_driver" placeholder="Name"/>
+            <input id="vehicle_contact" type="text" name="vehicle_contact" placeholder="Contact"/>
           </fieldset>
         </FormField>     
       </FormSection>
-      <FormSection id="guest-info">
+      <FormSection>
         <h2>Itinerary</h2>
         <FormField>
           <ItineraryEditor />

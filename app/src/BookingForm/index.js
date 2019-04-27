bab1f44aa49111ef3b766f594f7972b189a1c4c1
Tom Claudio <>
2019-04-27 17:03:35 +0800

    Refactor to JSX

--- app/src/BookingForm/index.js
+++ app/src/BookingForm/index.js
@@ -28,11 +28,11 @@ padding: 10px;
 export default props => (
   <React.Fragment>
     
-    <FormStyle className="pure-form">
+    <FormStyle classNameName="pure-form">
       <h1>Booking Form</h1>
       <FormSection>
         <FormField>
-          <label for="booking-type">Booking Type</label>
+          <label htmlFor="booking-type">Booking Type</label>
           <select id="booking-type">
             <option>Day tour</option>
             <option>Multi Day Tour</option>
@@ -42,53 +42,53 @@ export default props => (
       <FormSection>
         <h2>Contact Information</h2>
         <FormField>
-          <label for="name">Name: </label>
+          <label htmlFor="name">Name: </label>
           <input id="name" type="text" name="name" size={60}/>
         </FormField>
         <FormField>
-          <label for="name">Contact No: </label>
+          <label htmlFor="name">Contact No: </label>
           <input id="name" type="text" name="contact" size={60} />
         </FormField>
         <FormField>
-          <label for="name">Email: </label>
+          <label htmlFor="name">Email: </label>
           <input id="name" type="text" name="email" size={60} />
         </FormField>
       </FormSection>
       <FormSection>
         <h2>Guest Information</h2>
         <FormField>
-          <label for="name">Number of Adults: </label>
+          <label htmlFor="name">Number of Adults: </label>
           <input id="name" type="text" name="name" size={3} />
         </FormField>
         <FormField>
-          <label for="name">Number of kids(0-2): </label>
+          <label htmlFor="name">Number of kids(0-2): </label>
           <input id="name" type="text" name="name" size={3} />
           </FormField>
         <FormField>  
-          <label for="name">Number of kids(3-5): </label>
+          <label htmlFor="name">Number of kids(3-5): </label>
           <input id="name" type="text" name="name" size={3} />
           </FormField>
         <FormField>
-          <label for="name">Number of kids 6-11: </label>
+          <label htmlFor="name">Number of kids 6-11: </label>
           <input id="name" type="text" name="name" size={3} />
         </FormField>
         <FormField>
-          <label for="name">Arrival Date: </label>
-          <fieldset class="pure-group">
+          <label htmlFor="name">Arrival Date: </label>
+          <fieldset className="pure-group">
             <input id="name" type="text" name="email" />
             <input id="name" type="text" name="contact" placeholder="Flight Number"/>
           </fieldset>
         </FormField>
         <FormField>
-          <label for="name">Departurn Date: </label>
-          <fieldset class="pure-group">
+          <label htmlFor="name">Departurn Date: </label>
+          <fieldset className="pure-group">
             <input id="name" type="text" name="email" />
             <input id="name" type="text" name="contact" placeholder="Flight Number"/>
           </fieldset>
         </FormField>
         <FormField>
-          <label for="name">Tour Date: </label>
-          <fieldset class="pure-group">
+          <label htmlFor="name">Tour Date: </label>
+          <fieldset className="pure-group">
             <input id="name" type="text" name="email" />
             <input id="name" type="text" name="contact" placeholder="Start time"/>
             <input id="name" type="text" name="contact" placeholder="End time"/>
@@ -98,19 +98,19 @@ export default props => (
       <FormSection id="guest-info">
         <h2>Accommmodation Info</h2>
         <FormField>
-          <label for="name">Hotel: </label>
+          <label htmlFor="name">Hotel: </label>
           <input id="name" type="text" name="name" size={50} />
         </FormField>
         <FormField>
-          <label for="name">Reservation number</label>
+          <label htmlFor="name">Reservation number</label>
           <input id="name" type="text" name="contact"/>
         </FormField>
         <FormField>
-          <label for="name">Room number </label>
+          <label htmlFor="name">Room number </label>
           <input id="name" type="text" name="contact" size={5} />
         </FormField>
         <FormField>  
-          <label for="name">Reservation Type</label>
+          <label htmlFor="name">Reservation Type</label>
           <select id="booking-type">
             <option>Etours Account</option>
             <option>Guest Account</option>
@@ -120,7 +120,7 @@ export default props => (
       <FormSection id="guest-info">
         <h2>Airport Pickup</h2>
         <FormField>
-          <label for="name">With Airport Pickup</label>
+          <label htmlFor="name">With Airport Pickup</label>
           <input type="checkbox" />
         </FormField>
       </FormSection>
@@ -136,19 +136,19 @@ export default props => (
       <FormSection id="guest-info">
         <h2>Vehicle</h2>
         <FormField>
-          <label for="name">Operator: </label>
+          <label htmlFor="name">Operator: </label>
           <input id="name" type="text" name="contact" />
         </FormField>
         <FormField>    
-          <label for="name">Vehicle: </label>
-          <fieldset class="pure-group">
+          <label htmlFor="name">Vehicle: </label>
+          <fieldset className="pure-group">
             <input id="name" type="text" name="contact" placeholder="Type"/>
             <input id="name" type="text" name="contact" placeholder="Plate Number"/>
           </fieldset>
         </FormField>
         <FormField>    
-          <label for="name">Driver: </label>
-          <fieldset class="pure-group">
+          <label htmlFor="name">Driver: </label>
+          <fieldset className="pure-group">
             <input id="name" type="text" name="contact" placeholder="Name"/>
             <input id="name" type="text" name="contact" placeholder="Contact"/>
           </fieldset>

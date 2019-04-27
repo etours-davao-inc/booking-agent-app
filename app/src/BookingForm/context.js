978d6673ac1d76b889e88cece7498aa7356441a6
Tom Claudio <>
2019-04-28 02:03:26 +0800

    Create BookingContext

---
+++ app/src/BookingForm/context.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+const BookingContext = React.createContext();
+
+export class BookingProvider extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: "daytour"
+    }
+  }
+
+  onSelectChange(e) {
+    this.setState({[e.target.name]:e.target.value})
+  }
+  
+  render() {
+    return (
+      <BookingContext.Provider value={{
+          data: this.state,
+          actions: {
+            onSelectChange: this.onSelectChange.bind(this)
+          }
+        }}>
+        {this.props.children}
+      </BookingContext.Provider>
+    )
+  }
+}
+
+export const BookingConsumer = BookingContext.Consumer;

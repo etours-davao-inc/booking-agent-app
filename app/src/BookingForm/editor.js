ed59413883109569a0b33cbedb88ec0280e7238f
Tom Claudio <>
2019-04-27 16:58:38 +0800

    Create ItineraryBuilder

---
+++ app/src/BookingForm/editor.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import RichTextEditor from 'react-rte';
+import styled from 'styled-components';
+
+const EditorStyleWrapper = styled.span`
+  .RichTextEditor__root___2QXK- {
+    min-width: 960px;
+    min-height: 800px;
+  }
+`
+
+export default class ItineraryBuilder extends React.Component {
+  state = {
+    value: RichTextEditor.createEmptyValue()
+  }
+
+  onChange = (value) => {
+    this.setState({value});
+    if (this.props.onChange) {
+      // Send the changes up to the parent component as an HTML string.
+      // This is here to demonstrate using `.toString()` but in a real app it
+      // would be better to avoid generating a string on each change.
+      this.props.onChange(
+        value.toString('html')
+      );
+    }
+  };
+
+  render () {
+    return (
+      <EditorStyleWrapper>
+        <RichTextEditor value={this.state.value} onChange={this.onChange} />
+      </EditorStyleWrapper>
+    );
+  }
+}

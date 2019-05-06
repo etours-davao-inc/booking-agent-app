dd18e5964099aabc831c71206b959c1116bc0b61
Tom Claudio <>
2019-05-06 11:45:21 +0800

    Hook up input fields to context

--- app/src/BookingForm/NumPax/index.js
+++ app/src/BookingForm/NumPax/index.js
@@ -5,23 +5,24 @@ import { FormStyle } from '../Styled';
 
 export default () => {
   const { data, actions } = useContext(BookingContext);
+  const pax = data.userInput
   return (
     <fieldset>
       <div className="pure-control-group">
         <label htmlFor="adults">Number of Adults: </label>
-        <input id="adults" type="number" name="adults" style={{ width: '75px' }} />
+        <input id="adults" type="number" name="adults" value={pax.adults[0]} onChange={e => actions.onPaxChange(e)} />
       </div>
       <div className="pure-control-group">
         <label htmlFor="kid02">Number of kids (0-2): </label>
-        <input id="kid02" type="number" name="kid02" style={{ width: '75px' }} />
+        <input id="kid02" type="number" name="kid02" value={pax.kid02[0]} onChange={e => actions.onPaxChange(e)} />
       </div>
       <div className="pure-control-group">
         <label htmlFor="kid35">Number of kids (3-5): </label>
-        <input id="kid35" type="number" name="kid35" style={{ width: '75px' }} />
+        <input id="kid35" type="number" name="kid35" value={pax.kid35[0]} onChange={e => actions.onPaxChange(e)} />
       </div>
       <div className="pure-control-group">
         <label htmlFor="kid611">Number of kids (6-11): </label>
-        <input id="kid611" type="number" name="kid611" style={{ width: '75px' }} />
+        <input id="kid611" type="number" name="kid611" value={pax.kid611[0]} onChange={e => actions.onPaxChange(e)} />
       </div>
     </fieldset>
   )
--- app/src/BookingForm/Price/index.js
+++ app/src/BookingForm/Price/index.js
@@ -3,23 +3,24 @@ import { BookingContext } from '../context';
 
 export default () => {
   const { data, actions } = useContext(BookingContext);
+  const price = data.userInput;
   return (
     <fieldset>
       <div className="pure-control-group">
         <label htmlFor="adultprice">Adult Price</label>
-        <input id="adultprice" type="number" name="adultprice" />
+        <input id="adultprice" type="number" step="any" value={price.adults[1]} name="adults" onChange={e => actions.onPriceChange(e)}  />
       </div>
       <div className="pure-control-group">
         <label htmlFor="kid02price">Kid(0-2) Price</label>
-        <input id="kid02price" type="number" name="kid02price" />
+        <input id="kid02price" type="number" step="any" value={price.kid02[1]} name="kid02" onChange={e => actions.onPriceChange(e)}  />
       </div>
       <div className="pure-control-group">
         <label htmlFor="kid35price">Kid(3-5) Price</label>
-        <input id="kid35price" type="number" name="kid35price" />
+        <input id="kid35price" type="number" step="any" value={price.kid35[1]} name="kid35" onChange={e => actions.onPriceChange(e)}  />
       </div>
       <div className="pure-control-group">
         <label htmlFor="kid611price">Kid(6-11) Price</label>
-        <input id="kid611price" type="number" name="kid611price" />
+        <input id="kid611price" type="number" step="any"  value={price.kid611[1]} name="kid611" onChange={e => actions.onPriceChange(e)}  />
       </div>
     </fieldset>
   )

3afbac240f020cb459f6b12c3e6ee42ffa06c030
Tom Claudio <>
2019-05-06 11:45:56 +0800

    Extract styled div to parent module

--- app/src/BookingForm/Activities/index.js
+++ app/src/BookingForm/Activities/index.js
@@ -3,23 +3,11 @@ import { BookingContext } from '../context';
 
 import Activity from './Activity';
 
-import styled from 'styled-components';
-
-const ActivitiesWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 15px;
-`
-
 export default () => {
   const { data, actions } = useContext(BookingContext);
   var activities = [];
   for (let i = 0; i < data.days; i++) {
     activities.push(<Activity day={i} />)
   }
-  return (
-    <ActivitiesWrapper>
-      {activities}
-    </ActivitiesWrapper>
-  )
+  return activities
 }

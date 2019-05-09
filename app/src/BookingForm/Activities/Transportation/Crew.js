19abc3733f642bc4a4e004f665b5ad5f4134c3e2
Tom Claudio <>
2019-05-09 16:37:04 +0800

    Improve transportation module

--- app/src/BookingForm/Activities/Transportation/Crew.js
+++ app/src/BookingForm/Activities/Transportation/Crew.js
@@ -1,32 +1,79 @@
 import React, { useContext } from 'react';
 import { BookingContext } from '../../context';
-import { Grid3 } from '../../Styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
+
+import styled from 'styled-components';
+
+const AddCrewWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 5px;
+`
+
+const FaEdit = (props) => (<FontAwesomeIcon
+  icon={faEdit}
+  style={{ fontSize: '14px', color: 'rgba(99, 110, 114,1.0)', cursor: 'pointer', margin:'0 5px' }}
+  onClick={() => props.openModal()} />)
+
+const FaTrash = (props) => (<FontAwesomeIcon
+  icon={faTrashAlt}
+  style={{ fontSize: '14px', color: 'rgba(99, 110, 114,1.0)', cursor: 'pointer', margin:'0 5px' }}
+  onClick={() => props.openModal()} />)
 
 export default () => {
   const { data, actions } = useContext(BookingContext);
   return (
-    <React.Fragment>
-      <p>Crew</p>
-      <Grid3>
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input id="name" type="text" name="name" />
-        </div>
-        <div>
-          <label htmlFor="role">Role</label>
-          <select id="role" style={{ width: '187px' }}>
-            <option>Tour guide</option>
-            <option>Driver/Captain</option>
-            <option>Coordinator</option>
-            <option>Medic</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="contact">Contact</label>
-          <input id="contact" type="tel" name="contact" />
-        </div>
-      </Grid3>
-      <button>Add crew</button>
-    </React.Fragment>
+    <div>
+      <h4 style={{ margin: '15px 0 0 0' }}>Crew</h4>
+      <table class="pure-table pure-table-bordered" style={{ width: '100%', marginTop: "15px" }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Role</th>
+            <th>Contact</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jojo</td>
+            <td>Tourguide</td>
+            <td>09187011882</td>
+            <td style={{textAlign:'right'}}><FaEdit /><FaTrash/></td>
+          </tr>
+          <tr>
+            <td>Pedro</td>
+            <td>Drive</td>
+            <td>09187011882</td>
+            <td style={{textAlign:'right'}}><FaEdit /><FaTrash/></td>
+          </tr>
+        </tbody>
+      </table>
+      <div style={{ padding: '10px' }}>
+        <AddCrewWrapper>
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input id="name" type="text" name="name" />
+          </div>
+          <div>
+            <label htmlFor="role">Role</label>
+            <select id="role">
+              <option>Tour guide</option>
+              <option>Driver/Captain</option>
+              <option>Coordinator</option>
+              <option>Medic</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="contact">Contact</label>
+            <input id="contact" type="tel" name="contact" />
+          </div>
+          <fieldset style={{ marginTop: "20px" }}>
+            <button class="pure-button">Add</button>
+          </fieldset>
+        </AddCrewWrapper>
+      </div>
+    </div>
   )
 }
--- app/src/BookingForm/Activities/Transportation/index.js
+++ app/src/BookingForm/Activities/Transportation/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { BookingContext } from '../../context';
-import { Grid3, FormStyle } from '../../Styled';
+import { Grid3, FormStyle, FormBodyWrapper } from '../../Styled';
 
 import Crew from './Crew';
 
@@ -8,27 +8,35 @@ export default () => {
   const { data, actions } = useContext(BookingContext);
 
   return (
-    <FormStyle className="pure-form pure-form-aligned">
-      <Grid3>
-        <div>
-          <label htmlFor="vehicleType">Type: </label>
-          <select id="type" name="vehicleType" style={{ width: '187px' }}>
-            <option>Van</option>
-            <option>Bus</option>
-            <option>Banca</option>
-          </select>
+    <FormStyle className="pure-form pure-form-stacked">
+      <FormBodyWrapper>
+        <h2>Transportation Editor</h2>
+        <div style={{ border: '1px dashed silver', padding: '15px' }}>
+          <Grid3>
+            <div className="pure-control-group">
+              <label htmlFor="vehicleType">Type: </label>
+              <select id="vehicleType" name="vehicleType">
+                <option>Van</option>
+                <option>Bus</option>
+                <option>Banca</option>
+              </select>
+            </div>
+            <div className="pure-control-group">
+              <label htmlFor="identifier">Identifier </label>
+              <input id="identifier" type="text" name="identifier" placeholder="Plate Number" />
+            </div>
+            <div className="pure-control-group">
+              <label htmlFor="vehicle_operator">Operator: </label>
+              <input id="vehicle_operator" type="text" name="vehicle_operator" />
+            </div>
+          </Grid3>
+          <Crew />
         </div>
-        <div>
-          <label htmlFor="identifier">Identifier </label>
-          <input id="identifier" type="text" name="identifier" placeholder="Plate Number" />
-        </div>
-        <div>
-          <label htmlFor="vehicle_operator">Operator: </label>
-          <input id="vehicle_operator" type="text" name="vehicle_operator" />
-        </div>
-      </Grid3>
-      <Crew />
-      <button>Add Transportation</button>
+        <fieldset style={{ textAlign: 'right', marginTop:"35px" }}>
+          <button class="pure-button" onClick={() => actions.closeModal(false)}>Cancel</button>
+          <button type="submit" class="pure-button pure-button-primary">Add</button>
+        </fieldset>
+      </FormBodyWrapper>
     </FormStyle>
   )
 }

64ff72cf0447d09cfa2f98a6b3f8b43da7f203d4
Tom Claudio <>
2019-05-09 16:36:35 +0800

    Extract Editor

--- app/src/BookingForm/Activities/Itinerary/index.js
+++ app/src/BookingForm/Activities/Itinerary/index.js
@@ -1,43 +1,33 @@
-import React from 'react';
-import RichTextEditor from 'react-rte';
+import React, { useContext } from 'react';
+import RTEditor from './RTEditor';
 import styled from 'styled-components';
-
 import TimeLoc from './TimeLoc';
 
-import { FormStyle } from '../../Styled';
+import { FormStyle, FormBodyWrapper } from '../../Styled';
+
+import { BookingContext } from '../../context';
 
 const EditorStyleWrapper = styled.div`
-  padding: 0 35px;
   .RichTextEditor__root___2QXK- {
     min-height: 400px;
+    margin-bottom: 25px;
   }
 `
-
-export default class ItineraryEditor extends React.Component {
-  state = {
-    value: RichTextEditor.createEmptyValue()
-  }
-
-  onChange = (value) => {
-    this.setState({ value });
-    if (this.props.onChange) {
-      // Send the changes up to the parent component as an HTML string.
-      // This is here to demonstrate using `.toString()` but in a real app it
-      // would be better to avoid generating a string on each change.
-      this.props.onChange(
-        value.toString('html')
-      );
-    }
-  };
-
-  render() {
-    return (
-      <FormStyle className="pure-form pure-form-aligned">
+export default () => {
+  const { data, actions } = useContext(BookingContext);
+  return (
+    <FormStyle className="pure-form pure-form-stacked">
+      <FormBodyWrapper>
+        <h2>Itinerary Editor</h2>
         <TimeLoc />
         <EditorStyleWrapper>
-          <RichTextEditor value={this.state.value} onChange={this.onChange} />
+          <RTEditor />
         </EditorStyleWrapper>
-      </FormStyle>
-    );
-  }
+        <fieldset style={{ textAlign: 'right' }}>
+          <button class="pure-button" onClick={() => actions.closeModal(false)}>Cancel</button>
+          <button type="submit" class="pure-button pure-button-primary">Update</button>
+        </fieldset>
+      </FormBodyWrapper>
+    </FormStyle>
+  );
 }

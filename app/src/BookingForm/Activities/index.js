580ed6f421adc62615c3a6543317d713bbbf0476
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-08-28 14:42:30 +0800

    Remove unused import

--- app/src/BookingForm/Activities/index.js
+++ app/src/BookingForm/Activities/index.js
@@ -4,7 +4,7 @@ import { BookingContext } from '../context';
 import Activity from './Activity';
 
 export default () => {
-  const { data, actions } = useContext(BookingContext);
+  const { data } = useContext(BookingContext);
   var activities = [];
   for (let i = 0; i < data.days; i++) {
     activities.push(<Activity day={i} />)
